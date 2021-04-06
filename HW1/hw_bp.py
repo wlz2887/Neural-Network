542316332da15ba3c0c4fa5f38c31dacb43ed483
wlz2887 <70084038+wlz2887@users.noreply.github.com>
2021-04-06 11:06:38 +0800

    Update hw_bp.py

--- HW1/hw_bp.py
+++ HW1/hw_bp.py
@@ -4,6 +4,7 @@ from random import random
 from random import seed
 import math
 import matplotlib.pyplot as plt
+import time
 
 #input_layer:输入值
 #hidden:隐藏层计算的结果
@@ -49,39 +50,40 @@ rate = 0.03
 Epoch = 5000
 loss = []
 
-def train(rate,Epoch):
-    for times in range(0,Epoch):
-        Loss=0
-        for i in range(0,194): #on-line learning
-            input_layer=x[i]
-            true_val=y[i]
-            hidden = np.dot(v1,input_layer)+np.dot(u1,input_layer**2)+Bias[0]
-            hidden = sigmoid(hidden)#计算隐藏层
-            hidden = hidden.reshape(n_hidden,1)
-            output = sigmoid(np.dot(v2,hidden)+np.dot(u2,hidden**2)+Bias[1])#计算输出层，结束前向计算
-            
-            #预处理
-            input_layer = input_layer.reshape(n_input,1)
-            deriv1 = sigmoidDeriviation(hidden) #hidden(1-hidden)
-            deriv2 = sigmoidDeriviation(output) #output(1-output)
-            delta2 = -np.multiply((true_val-output),deriv2)#(e*out*(1-out))
-            coeff = np.dot(delta2.T,v2)+np.dot(delta2.T,np.multiply(u2.T,hidden).T)#输出层产生的梯度
-            
-            #反向传播
-            tmp3 = np.multiply(coeff.T,deriv1)
-            v1 -= rate*(np.dot(tmp3,input_layer.T)) 
-            u1 -= rate*(np.dot(tmp3,(input_layer**2).T))
+start = time.perf_counter()
+for times in range(0,Epoch):
+    Loss=0
+    for i in range(0,194): #on-line learning
+        input_layer=x[i]
+        true_val=y[i]
+        hidden = np.dot(v1,input_layer)+np.dot(u1,input_layer**2)+Bias[0]
+        hidden = sigmoid(hidden)#计算隐藏层
+        hidden = hidden.reshape(n_hidden,1)
+        output = sigmoid(np.dot(v2,hidden)+np.dot(u2,hidden**2)+Bias[1])#计算输出层，结束前向计算
 
-            tmp1 = np.dot(delta2,hidden.T)
-            tmp2 = np.dot(delta2,(hidden**2).T)
-            v2 -= rate*tmp1
-            u2 -= rate*tmp2
+        #预处理
+        input_layer = input_layer.reshape(n_input,1)
+        deriv1 = sigmoidDeriviation(hidden) #hidden(1-hidden)
+        deriv2 = sigmoidDeriviation(output) #output(1-output)
+        delta2 = -np.multiply((true_val-output),deriv2)#(e*out*(1-out))
+        coeff = np.dot(delta2.T,v2)+np.dot(delta2.T,np.multiply(u2.T,hidden).T)#输出层产生的梯度
 
-            #记录loss变化
-            Loss += 0.5*(true_val-output)**2 
-            if(i==193 and times % 100 == 0): #you can change the interval of recording loss as you wish
-                print('The loss of Epoch ',times,' is ',Loss/194)
-                loss.append(Loss[0]/194)
+        #反向传播
+        tmp3 = np.multiply(coeff.T,deriv1)
+        v1 -= rate*(np.dot(tmp3,input_layer.T)) 
+        u1 -= rate*(np.dot(tmp3,(input_layer**2).T))
+
+        tmp1 = np.dot(delta2,hidden.T)
+        tmp2 = np.dot(delta2,(hidden**2).T)
+        v2 -= rate*tmp1
+        u2 -= rate*tmp2
+
+        #记录loss变化
+        Loss += 0.5*(true_val-output)**2 
+        if(i==193): #you can change the interval of recording loss as you wish
+            loss.append(Loss[0]/194)
+end = time.perf_counter()
+print(start-end)
 
 def validation():#作图验证
     I=np.linspace(-6,6,100)
@@ -98,5 +100,4 @@ def validation():#作图验证
             else:
                 plt.scatter(i,j,c='blue',s=2)
                 
-train(0.03,5000)
 validation()
