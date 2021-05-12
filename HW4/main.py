792b564241edb9c81d4f1c8e4666ac27c7a14435
wlz2887 <70084038+wlz2887@users.noreply.github.com>
2021-05-12 11:07:23 +0800

    Update main.py

--- HW4/main.py
+++ HW4/main.py
@@ -11,12 +11,15 @@ from sklearn.preprocessing import StandardScaler
 train_data = np.load('./train_data.npy')
 test_data = np.load('./test_data.npy')
 train_label = np.load('./train_label.npy')
+test_label = np.load('./test_label.npy')
+
+#Normalization
 scaler = StandardScaler()
 scaler.fit(train_data)
 train_data = scaler.transform(train_data)
 test_data = scaler.transform(test_data)
 
-test_label = np.load('./test_label.npy')
+
 # train_label = torch.from_numpy(train_label+1).long()
 # test_label = torch.from_numpy(test_label+1).long()
 # train_data = torch.from_numpy(train_data)
@@ -53,13 +56,13 @@ for i in range(0,4):
     dat = torch.from_numpy(np.array(dat)).double()
     tag = torch.from_numpy(np.array(tag)).long()
     test = TensorDataset(dat,tag)
-    test_loader = DataLoader(dataset = test, shuffle = False, batch_size = 1,num_workers = 8)
+    test_loader = DataLoader(dataset = test, shuffle = False, batch_size = 4,num_workers = 8)
     loader2.append(test_loader)
 
 #============================================================================================
 #train
 
-batch_size = 1
+batch_size = 4
 net = model(310,64,batch_size,6) #seq_len and hidden_dim, batch_size is set to default 1
 net = net.double()
 optimizer = optim.Adam(net.parameters(),lr = 0.0001)
@@ -76,45 +79,26 @@ for epoch in range(100):
         loader_iter = iter(Loader)
         for j in range(len(Loader)):
             data,label = loader_iter.next()
-            label = label.squeeze(dim=0)
-            net.hidden = net.init_hidden()
-            input_size = len(data)
-
+            Batch_size = len(data)
+            label = label.view(Batch_size*43,-1).squeeze(dim=1)
+            net.hidden = net.init_hidden(Batch_size)
+#             input_size = len(data)
             class_out = net(data)
             loss = loss_func(class_out,label)
 
             loss.backward()
             optimizer.step()
     print('epoch = ',epoch,' loss = ',loss)
-    
+
     true_class = 0
     for i in range(11):
         Loader = loader[i]
         loader_iter = iter(Loader)
         for j in range(len(Loader)):
             data,label = loader_iter.next()
-            label = label.squeeze(dim=0)
-
-            label = label.squeeze(dim=0)
-            net.hidden = net.init_hidden()
-            input_size = len(data)
-
-            class_out = net(data)
-            loss = loss_func(class_out,label)
-
-            loss.backward()
-            optimizer.step()
-            #if(j%5 == 0):
-             #   print('data ',j/5, 'loss = ',loss)
-    print('epoch = ',epoch,' loss = ',loss)
-    true_class = 0
-    for i in range(11):
-        Loader = loader[i]
-        loader_iter = iter(Loader)
-        for j in range(len(Loader)):
-            data,label = loader_iter.next()
-            label = label.squeeze(dim=0)
-            net.hidden = net.init_hidden()
+            Batch_size = len(data)
+            label = label.view(Batch_size*43,-1).squeeze(dim=1)
+            net.hidden = net.init_hidden(Batch_size)
             input_size = len(data)
             class_out = net(data)
             out_label = torch.argmax(class_out,dim=1)
@@ -127,9 +111,10 @@ for epoch in range(100):
         loader_iter = iter(Loader)
         for j in range(len(Loader)):
             data,label = loader_iter.next()
-            label = label.squeeze(dim=0)
-            net.hidden = net.init_hidden()
-            input_size = len(data)
+            Batch_size = len(data)
+            label = label.view(43*Batch_size,-1).squeeze(dim=0)
+            net.hidden = net.init_hidden(Batch_size)
+            
             class_out = net(data)
             out_label = torch.argmax(class_out,dim=1)
             true_class += (out_label==label).sum().item()
